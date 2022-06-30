afe1800c2cd237252595d949d08a82fec2f16347
Matthias Schlecht <mail-github@matthias-schlecht.de>
2022-06-30 16:48:36 +0200

    first commit

---
+++ display.py
@@ -0,0 +1,239 @@
+
+#!/usr/bin/python3
+
+# ----------------------------------------------------- #
+#                                                       #
+# Skript zum Anzeigen von Janiza UMG96 - Logs           #
+# Konfiguration erfolgt am unteren Ende :-)             #
+#                                                       #
+# Bei Fragen: mail-janiza@f50hz.de                      #
+#                                                       #
+# Matthias Schlecht 2022                                #
+#                                                       #
+# ----------------------------------------------------- #
+
+from rich.console import Console
+from rich.table import Table
+from rich.layout import Layout
+from rich.panel import Panel
+from rich.live import Live
+from rich.console import Group
+from datetime import datetime
+from datetime import timedelta
+import time
+import os
+
+
+def get_data(filename):
+  # Methode zu, auslesen der letzten Zeile einer Datei und Parsen der Werte
+  #  filename: Das zu lesende File.
+  
+  # Datei öffnen und letzte Zeile auslesen.
+  with open(filename, 'r') as f:
+    last_line = f.readlines()[-1]
+    csv=last_line.split(",") # Splitten anhand des Trenners ","
+  
+  # Datum mal direkt ordentlich als datetime-Objekt anlegen.
+  datecsv= datetime.strptime(csv[0],'%Y_%m_%d-%H:%M:%S.%f')
+  
+  # Größe der Datei ermitteln
+  fileinfo=os.stat(filename)
+  filesize=str(round(fileinfo.st_size/(1024*1024),1))
+
+  # Wenn die letzte Zeile zu alt ist, dann Warnung einfügen.
+  if (datetime.now()-datecsv) > timedelta(seconds=10):
+     warning="[blink]!! OLD DATA !!"
+  else:
+     warning=""
+  
+  # Aus dem CSV-String ein dict bauen.
+  data = {
+   "date": datecsv,
+   "u1": csv[1],
+   "u2": csv[2],
+   "u3": csv[3],
+   "u12": csv[4],
+   "u23": csv[5],
+   "u31": csv[6],
+   "i1": csv[7],
+   "i2": csv[8],
+   "i3": csv[9],
+   "iN": csv[10],
+   "p1": csv[11],
+   "p2": csv[12],
+   "p3": csv[13],
+   "freq": csv[14],
+   "cos1": csv[15],
+   "cos2": csv[16],
+   "cos3": csv[17],
+   "cosS": csv[18],
+   "drehfeld": "-",
+   "energy": csv[19][:-1],
+   "warning": warning,
+   "filesize": filesize
+  }
+
+  return data
+    
+
+def print_panel(filename,title,maxcurrent) -> Panel:
+  # Methode um ein Rich-Panel mit der Tabelle der Werte zu generieren.
+  # filename:   Dateiname der zu lesenden Datei
+  # title:      Überschrift des Panels
+  # maxcurrent: Stromschwelle für die Einfärbung der Phasen
+  
+  
+  # Ein paar generelle Variablen für Einheiten und Einfärbungen.
+  unitV="V"
+  unitI="A"
+  unitP="W"
+  unitF="Hz"
+  iyellow=maxcurrent*0.8
+  ired=maxcurrent*0.95
+  l1warn = l2warn = l3warn = lNwarn =  "[green]"
+
+  # Dict mit den Daten holen.
+  data = get_data(filename)
+
+  # Bestimmen ob Stromwerte überschritten sind
+  if float(data["i1"]) > ired:
+   l1warn = "[bold bright_red]"
+  elif float(data["i1"]) > iyellow:
+   l1warn = "[bold bright_yellow]"
+
+  if float(data["i2"]) > ired:
+   l2warn = "[bold bright_red]"
+  elif float(data["i2"]) > iyellow:
+   l2warn = "[bold bright_yellow]"
+
+  if float(data["i3"]) > ired:
+   l3warn = "[bold bright_red]"
+  elif float(data["i3"]) > iyellow:
+   l3warn = "[bold bright_yellow]"
+
+  if float(data["iN"]) > ired:
+   lNwarn = "[bold bright_red]"
+  elif float(data["iN"]) > iyellow:
+   lNwarn = "[bold bright_yellow]"
+
+  # Zusammenbasteln der Tabellen. Rich-Tables können keinen Col-Span daher drei Tabellen die gruppiert werden.
+  header = Table.grid(expand=False,padding=(0,0,0,1))
+  header.add_column()
+  header.add_row("Last Value: "+datetime.strftime(data["date"],'%d.%m.%Y %H:%M:%S'))
+  header.add_row("[red]"+data["warning"])
+
+
+  table = Table.grid(expand=False,padding=(0,0,0,1))
+  table.add_column(width=3)
+  table.add_column(width=7)
+  table.add_column(width=7)
+  table.add_column(width=8)
+  table.add_row("L1:",data["u1"]+unitV,"L1-L2:",data["u12"]+unitV)
+  table.add_row("L2:",data["u2"]+unitV,"L2-L3:",data["u23"]+unitV)
+  table.add_row("L3:",data["u3"]+unitV,"L3-L1:",data["u31"]+unitV)
+  table.add_row("F:",data["freq"]+unitF,"cosPhi:",data["cosS"])
+  table.add_row()
+  table.add_row("I1:",l1warn+data["i1"]+unitI,"P1:",data["p1"]+unitP)
+  table.add_row("I2:",l2warn+data["i2"]+unitI,"P2:",data["p2"]+unitP)
+  table.add_row("I3:",l3warn+data["i3"]+unitI,"P3:",data["p3"]+unitP)
+  table.add_row("IN:",lNwarn+data["iN"]+unitI)
+  
+  footer= Table.grid(expand=False,padding=(0,0,0,0))
+  footer.add_column()
+  footer.add_row()
+  footer.add_row("Filesize: "+data["filesize"]+"M")
+   
+  #Tabellen gruppieren
+  panel_group=Group(
+     header,
+     table,
+     footer,
+  )
+  
+  #Panel-Objekt bauen und mit Titel versehen.
+  panel=Panel.fit(panel_group,title=title)
+  return panel
+
+
+def make_Layout() -> Layout:
+# Methode zum Erstellen des Grund-Layouts
+  lay=Layout()
+  lay.split_column(
+    Layout(name="header"), 
+    Layout(name="row1"), 
+    Layout(name="row2"), 
+    Layout(name="footer"),
+  )
+  lay["row1"].split_row(
+    Layout(name="UMG1"),
+    Layout(name="UMG2"),
+  )
+  lay["row2"].split_row(
+    Layout(name="UMG3"),
+    Layout(name="UMG4"),
+  )
+  lay["header"].size=3
+  lay["footer"].size=3
+  lay["row1"].size=15
+  lay["row2"].size=15
+
+  return lay
+
+def print_Header() -> Panel:
+# Methode zum Erstellen des Header-Panels
+
+  headertext="JANITZA UMG96 Strommonitoring"
+  header=Panel(headertext)
+
+  return header
+
+def print_Footer(text) -> Panel:
+# Methode zum Erstellen des Footer-Panels
+
+  headertext=datetime.now().strftime('%d.%m.%Y %H:%M:%S')+" [bold red]"+text
+  header=Panel(headertext)
+
+  return header
+
+
+# Ab hier kommet das Haupt-Programm
+
+# Grund-Layout erstellen
+lay=make_Layout()
+# Konsole instanzieren.
+console = Console()
+
+# ------------------------------------------------------------------------- #
+#
+#  Ab hier gehts los mit der Konfig.
+#
+# ------------------------------------------------------------------------- # 
+
+#Live-Objekt instanziern
+with Live(lay, refresh_per_second=1) as live:
+    while (1):
+        # Und los: 
+        try:
+          # Hier werden die 4 Panels mit Inhalt gefüllt
+          #                                filenme             Name             Strom
+          lay["UMG1"].update(print_panel("A010_powerdata.log","TEST 16A",16))
+          #lay["UMG2"].update(print_panel("A002_powerdata.log","Benenn Mich 16A",16))
+          #lay["UMG3"].update(print_panel("A100_powerdata.log","Benenn Mich 16A",16))
+          #lay["UMG4"].update(print_panel("/tmp/testdaten.log","Ton 5A",5))
+        
+          # Header und Footer aktualisieren
+          lay["header"].update(print_Header())
+          lay["footer"].update(print_Footer(""))          
+          
+          # Anti-Eskalations-Sleep
+          time.sleep(0.4)
+          
+        # Wenn was schief geht:
+        except Exception as excep:
+          # Nur den Header und den Footer aktualisiern und die exception in den Footer schreiben. So bleiben die alten Werte stehen und das Display flackert nicht.
+          lay["header"].update(print_Header())
+          lay["footer"].update(print_Footer("Fehler!:"+str(excep)))
+        # Wenn jemand STRG+C macht ordentlich beenden.
+        except KeyboardInterrupt:
+          break
+quit()
---
+++ logger.py
@@ -0,0 +1,217 @@
+#!/usr/bin/python3
+
+# ----------------------------------------------------- #
+#                                                       #
+# Skript zum Auslesen von Janiza UMG96                  #
+# Konfiguration erfolgt am unteren Ende :-)             #
+#                                                       #
+# Bei Fragen: mail-janiza@f50hz.de                      #
+#                                                       #
+# Matthias Schlecht 2022                                #
+#                                                       #
+# ----------------------------------------------------- #
+
+
+from pymodbus.client.sync import ModbusSerialClient as mbc
+from datetime import datetime
+from time import sleep
+import os
+
+def ieee2float(reg1, reg2):
+  #Methode zum IEEE754-Floats in python Floats zu konvertieren
+  binaer = reg1 <<16 | reg2
+  vz = binaer >> 31 
+  expb = (binaer&0b01111111100000000000000000000000)>>23
+  man = (binaer&0b00000000011111111111111111111111)
+  bias = 127
+  exp = expb-bias
+  norm = (man/(0b1<<23))+1
+  ret = (norm*(0b1<<exp))*((-1)**vz)
+  return ret
+
+
+def twoCompl(zahl):
+    # Einmal 2-er-Komplement bitte
+    if zahl > 0x8FFF:
+        zahl=zahl-0xFFFF
+    return zahl
+
+
+#Modbus-Foo
+
+def establish_modbus(tty):
+    #Diese Methode baut ein Objekt mit dem Modbus-Client und gibt es zurück, dass an die Logger-Methoden übergeben wird.
+    client = mbc(method="rtu", port=tty, stopbits=1, bytesize=8, parity='N', baudrate=9600)
+    return client
+
+
+def get_modbus(client,startadd,numofreg,address):
+  #Abfrage von mehreren Registern sequentiell über ein Modbus-Client-Objekt.
+  #  client: Modbus-Client-Objekt
+  #  startadd: Startadresse
+  #  numofreg: Anzahl der zu lesenden Registern
+  #  address: Modbus-Adresse
+  client.connect()
+  read = client.read_holding_registers(startadd, numofreg, unit=address)
+  client.close()
+  sleep(0.05)
+  return read.registers
+
+def logger_UMG96RM(client,address,filename):
+     #Logger-Methode für das UMG96RM.
+     #  client: Modbus-Client-Objekt
+     #  address: Modbus-Adresse
+     #  filename: Log-File
+     
+     # Definition von Faktoren für Spannung, Strom, Leistung und cos(phi)
+     factu=0.1
+     facti=(100/5)*0.001
+     factp=(100/5)*factu
+     factcos=0.01
+
+     # Lesen der Registerblöcke
+     
+     # Frequenz
+     freqr = get_modbus(800,2,address)
+     
+     # L1,L2,L3, L1-L2, L2-L3, L3-L1
+     valuesU = get_modbus(3530,6,address)
+
+     # I1, I2, I3, IN(calc), P1,P2,P3,Psum 
+     valuesIP = get_modbus(3916,8,address)
+
+     # Cos(phi)
+     valuesCos = get_modbus(3776,4,address)
+     # Energie
+     valuesEn  = get_modbus(19060,2,address)
+
+     date = datetime.now().strftime('%Y_%m_%d-%H:%M:%S.%f')
+    
+     # Runden und Konvertieren der Werte 
+     freq = round(ieee2float(freqr[0],freqr[1]),5)
+     u1= round(valuesU[0]*factu,1)
+     u2= round(valuesU[1]*factu,1)
+     u3= round(valuesU[2]*factu,1)
+     u12= round(valuesU[3]*factu,1)
+     u23= round(valuesU[4]*factu,1)
+     u31= round(valuesU[5]*factu,1)
+     cos1=round(twoCompl(valuesCos[0])*factcos,3)
+     cos2=round(twoCompl(valuesCos[1])*factcos,3)
+     cos3=round(twoCompl(valuesCos[2])*factcos,3)
+     cosSum=round(twoCompl(valuesCos[3])*factcos,10)
+
+     i1=round(valuesIP[0]*facti,3)
+     i2=round(valuesIP[1]*facti,3)
+     i3=round(valuesIP[2]*facti,3)
+     iN=round(valuesIP[3]*facti,3)
+     p1=round(valuesIP[4]*factp,0)
+     p2=round(valuesIP[5]*factp,0)
+     p3=round(valuesIP[6]*factp,0)
+     
+     energy=round(ieee2float(valuesEn[0],valuesEn[1])/1000,3)
+
+     # gepfuschtes Zusammenbauen einer CSV. Ja man könnte das mit einem Dict machen.
+     value = date+","+str(u1)+","+str(u2)+","+str(u3)+","+str(u12)+","+str(u23)+","+str(u31)+","+str(i1)+","+str(i2)+","+str(i3)+","+str(iN)+","+str(p1)+","+str(p2)+","+str(p3)+","+str(freq)+","+str(cos1)+","+str(cos2)+","+str(cos3)+","+str(cosSum)+","+str(energy)
+
+     # Speichern der Log-Zeile
+     with open(filename, "a") as myfile:
+      myfile.write(value+"\n")
+      myfile.close()
+
+def logger_UMG96S(client,address,filename):
+     # Logger-Methode für das UMG96S
+     
+     # Definition von Faktoren für Spannung, Strom, Leistung und cos(phi)
+     factu=0.1
+     facti=(100/5)*0.001
+     factp=(100/5)*factu
+     factcos=0.01
+     factf=0.01
+      
+     # Lesen der Registerblöcke
+     # Frequenz
+     freqr = get_modbus(client,800,2,address)
+     
+     # Spannung, Stom, Leistung
+     valuesUIP = get_modbus(client,200,21,address)
+     # Summen der Phasen und iN
+     valuesSums = get_modbus(client,275,5,address)
+     # Energie
+     valuesEn = get_modbus(client,416,2,address)
+
+     date = datetime.now().strftime('%Y_%m_%d-%H:%M:%S.%f')
+     
+      # Runden und Konvertieren der Werte 
+     freq = round(valuesSums[0]*factf,2)
+     u1= round(valuesUIP[0]*factu,1)
+     u2= round(valuesUIP[1]*factu,1)
+     u3= round(valuesUIP[2]*factu,1)
+     u12= round(valuesUIP[3]*factu,1)
+     u23= round(valuesUIP[4]*factu,1)
+     u31= round(valuesUIP[5]*factu,1)
+     cos1=round(twoCompl(valuesUIP[18])*factcos,3)
+     cos2=round(twoCompl(valuesUIP[19])*factcos,3)
+     cos3=round(twoCompl(valuesUIP[20])*factcos,3)
+
+     cosSum=round(twoCompl(valuesSums[1])*factcos,3)
+
+     i1=round(valuesUIP[6]*facti,3)
+     i2=round(valuesUIP[7]*facti,3)
+     i3=round(valuesUIP[8]*facti,3)
+     iN=round(valuesSums[3]*facti,3)
+     p1=round(valuesUIP[9]*factp,0)
+     p2=round(valuesUIP[10]*factp,0)
+     p3=round(valuesUIP[11]*factp,0)
+     energy=round(((valuesEn[0]*0x10000)+valuesEn[1])*factp*0.01,3)
+
+ 
+     # gepfuschtes Zusammenbauen einer CSV. Ja man könnte das mit einem Dict machen.
+     value = date+","+str(u1)+","+str(u2)+","+str(u3)+","+str(u12)+","+str(u23)+","+str(u31)+","+str(i1)+","+str(i2)+","+str(i3)+","+str(iN)+","+str(p1)+","+str(p2)+","+str(p3)+","+str(freq)+","+str(cos1)+","+str(cos2)+","+str(cos3)+","+str(cosSum)+","+str(energy)
+     
+     # Speichern der Log-Zeile
+     with open(filename, "a") as myfile:
+      myfile.write(value+"\n")
+      myfile.close()
+
+# Ab hier kommet das Haupt-Programm
+
+# Zuerst mal die Konsole leeren....
+os.system('clear')
+
+# ------------------------------------------------------------------------- #
+#
+#  Ab hier gehts los mit der Konfig.
+#
+# ------------------------------------------------------------------------- # 
+
+# Modbus-Client-Objekt instanzieren 
+# Hier könnte man das tty-Device ändern.
+client=establish_modbus("/dev/ttyUSB0")
+
+while True:
+    
+    # Beispiele für Filenames mit Datum drin, wenn man mal länger loggen will.
+
+    filename1 = datetime.now().strftime('powerdata_100'+'_%Y_%m_%d.log')
+    
+    # Und los: Einmal eine Methodenaufruf für jedes Janiza. Danach eine kurze Statusausgabe.
+    try:
+       logger_UMG96S(client,10,"A010_powerdata.log")
+       #logger_UMG96S(1,"A001_powerdata.log")
+       #logger_UMG96RM(100,"A100_powerdata.log")
+       
+       
+       print("Last Log: "+datetime.now().strftime('%H:%M:%S'))
+    
+    # Wenn irgendwas schief geht: Fehler ausgeben und 500ms warten.
+    except Exception as err:
+       #os.system('clear')
+       print('oops... Es hat mal wieder nicht geklappt...')
+       sleep(0.5)
+       print(str(err))
+       pass
+   
+    # Wenn jemand STRG+C macht ordentlich beenden.
+    except KeyboardInterrupt:
+       break
+quit()
